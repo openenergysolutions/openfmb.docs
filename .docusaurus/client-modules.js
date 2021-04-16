aa8fa5ef6188407530b06bc0ad51c39be1943274
Tom Burdick <thomas.burdick@gmail.com>
2021-04-16 10:13:54 -0500

    Initial docusaurus docs site for openfmb

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/code/tburdick/src/openfmb.docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/code/tburdick/src/openfmb.docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/code/tburdick/src/openfmb.docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/code/tburdick/src/openfmb.docs/node_modules/infima/dist/css/default/default.css"),
+  require("/code/tburdick/src/openfmb.docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/code/tburdick/src/openfmb.docs/src/css/custom.css"),
+];
