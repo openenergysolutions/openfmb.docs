4a0f290d906b00283058eaa11bbeef85d78cad14
Tom Burdick <thomas.burdick@gmail.com>
2021-04-22 13:17:45 -0500

    Remove generated .docusaurus from repo

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/code/tburdick/src/openfmb.docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/code/tburdick/src/openfmb.docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/code/tburdick/src/openfmb.docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/code/tburdick/src/openfmb.docs/node_modules/infima/dist/css/default/default.css"),
-  require("/code/tburdick/src/openfmb.docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/code/tburdick/src/openfmb.docs/src/css/custom.css"),
-];
