bd6b2ef510348f1aed882bab1ff29d7b1bbf88b6
Cory Nguyen <corynguyenfl@gmail.com>
2024-02-21 09:26:10 -0500

    Added "OpenFMB v2.1" text on main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,9 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+      <div className="container">        
         <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title">OpenFMB v2.1</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
